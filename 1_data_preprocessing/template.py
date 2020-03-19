aa5942c2193611a07ba9f77f973188e99064e4e7
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-19 17:09:42 +0000

    data pre-processing complete.

--- 1_data_preprocessing/template.py
+++ 1_data_preprocessing/template.py
@@ -32,9 +32,8 @@ x[:, 1:3] = imp_mean.transform(x[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-
 # Deprecated version of handling categorical data.
-# ===============================================
+# ************************************************
 # le_x = LabelEncoder()
 # # Fit the label encoder to the data of interest.
 # le_x = le_x.fit(x[:, 0])
@@ -46,7 +45,7 @@ from sklearn.compose import ColumnTransformer
 
 
 # Newer way of converting Categorical Data
-# ========================================
+# ****************************************
 # Create a column transformer using a OneHotEncoder to convert
 # Categorical Data to Binary Encoded data.
 ct = ColumnTransformer(
@@ -59,3 +58,19 @@ x = np.array(ct.fit_transform(x), dtype=np.int32)
 # Convert Y category (Yes/No) to binary encoding.
 y = np.array(ct.fit_transform(y), dtype=np.int32)
 
+# Splitting the dataset into a training set and a test set
+# ========================================================
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.20, random_state=0
+)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+
+# Scale the x_train, x_test datasets.
+x_train[:, 3:] = sc_X.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc_X.transform(x_test[:, 3:])  # Dont re-fit as X_train cols == X_test cols
