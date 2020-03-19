01589cf14ea8290a32d89edd52eb2f2f9b92326d
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-19 14:42:33 +0000

    Handled missing numerical data using SimpleImputer.

--- 1_data_preprocessing/template.py
+++ 1_data_preprocessing/template.py
@@ -8,8 +8,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# import dataset
-dataset = pd.read_csv("./1_data_preprocessing/Data.csv")
+# Takes care of missing data.
+from sklearn.impute import SimpleImputer
 
+# import dataset
+# ==============
+dataset = pd.read_csv("./1_data_preprocessing/Data.csv")
 # Select all rows, select all columns except the last.
 x = dataset.iloc[:, :-1].values
+# Select all rows, select only the 4th column.
+y = dataset.iloc[:, 3].values
+
+# Handle missing data.
+# ====================
+imp_mean = SimpleImputer(missing_values=np.nan, strategy="mean")
+# Fit the imputer to the size to of the dataset of interest.
+imp_mean = imp_mean.fit(x[:, 1:3])
+# Fill the missing data and reassign data to original dataset.
+x[:, 1:3] = imp_mean.transform(x[:, 1:3])
+
+
