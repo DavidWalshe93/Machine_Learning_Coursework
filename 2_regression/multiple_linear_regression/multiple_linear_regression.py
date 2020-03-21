8a09a14ead9a0eae4d84ef28cb28dc6062b70d20
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-21 10:53:22 +0000

    Added constant of multiple linear regression back into X to perform Backwards Elimination.

--- 2_regression/multiple_linear_regression/multiple_linear_regression.py
+++ 2_regression/multiple_linear_regression/multiple_linear_regression.py
@@ -40,7 +40,6 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-
 # Fitting Multiple Linear Regression to the Training set
 # ======================================================
 from sklearn.linear_model import LinearRegression
@@ -49,8 +48,14 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 
-
 # Predicting the Test set results
 # ===============================
 y_pred = regressor.predict(x_test)
 
+# Using backward elimination to generate a better model.
+# ======================================================
+import statsmodels.formula.api as sm
+
+# Add an array of ones to the start of X to account for constant, C0 in multiple linear regression equation.
+# Y = C0 + C1*X1 + C2*X2 + ... + Cn*Xn
+x = np.append(arr=np.ones((50, 1)).astype(int), values=x, axis=1)
