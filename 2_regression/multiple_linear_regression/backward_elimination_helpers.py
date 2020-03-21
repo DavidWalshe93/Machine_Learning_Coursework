05bc2ad9010e123286f9b0677b220e8386dcceb4
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-21 15:00:21 +0000

    Added some helper functions for backward elimination automation.

---
+++ 2_regression/multiple_linear_regression/backward_elimination_helpers.py
@@ -0,0 +1,73 @@
+"""
+Author:         David Walshe
+Date:           21/03/2020   
+"""
+
+import statsmodels.formula.api as sm
+import numpy as np
+
+
+def backwardElimination(x, y, sl):
+    """
+    Backward Elimination using p-value.
+
+    :param x: The table of features.
+    :param y: The dependant variable.
+    :param sl: The significant value.
+    :return: The table of values with a p-value of 0.05 or less.
+    """
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+
+# Usage
+# =====
+# sl = 0.05
+# X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# X_Modeled = backwardElimination(X_opt, sl)
+
+def backwardEliminationAdjR(x, y, sl):
+    """
+    Backward Elimination using on p-value and adjusted R squared value.
+
+    :param x: The table of features.
+    :param y: The dependant variable.
+    :param sl: The significant value.
+    :return: The table of values with a p-value of 0.05 or less.
+    """
+    numVars = len(x[0])
+    temp = np.zeros((50, 6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:, j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:, [0, j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print(regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
+
+# Usage
+# =====
+# sl = 0.05
+# X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# X_Modeled = backwardElimination(X_opt, sl)
--- 2_regression/multiple_linear_regression/multiple_linear_regression.py
+++ 2_regression/multiple_linear_regression/multiple_linear_regression.py
@@ -34,6 +34,10 @@ ct = ColumnTransformer(
 # Run the transformer on X.
 x = np.array(ct.fit_transform(x), dtype=np.float64)
 
+# Avoid the dummy variable trap
+# =============================
+x = x[:, 1:]
+
 # Split training and test sets
 # ============================
 from sklearn.model_selection import train_test_split
@@ -59,3 +63,55 @@ import statsmodels.formula.api as sm
 # Add an array of ones to the start of X to account for constant, C0 in multiple linear regression equation.
 # Y = C0 + C1*X1 + C2*X2 + ... + Cn*Xn
 x = np.append(arr=np.ones((50, 1)).astype(int), values=x, axis=1)
+
+# Decide a significance level.
+sig_level = 0.05
+
+
+# Phase 1
+# =======
+# The initial optimised table of features.
+x_opt = x[:, [0, 1, 2, 3, 4, 5]]
+
+# Ordinarily Least Squares Regressor.
+regressor_OLS = sm.OLS(endog=y, exog=x_opt).fit()
+# regressor_OLS.summary()
+
+
+# Phase 2
+# =======
+# Removed least significant P-value [2] found in Phase 1.
+x_opt = x[:, [0, 1, 3, 4, 5]]
+
+# Ordinarily Least Squares Regressor.
+regressor_OLS = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_OLS.summary()
+
+
+# Phase 3
+# =======
+# Removed least significant P-value [1] found in Phase 1.
+x_opt = x[:, [0, 3, 4, 5]]
+
+# Ordinarily Least Squares Regressor.
+regressor_OLS = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_OLS.summary()
+
+
+# Phase 4
+# =======
+# Removed least significant P-value [4] found in Phase 1.
+x_opt = x[:, [0, 3, 5]]
+
+# Ordinarily Least Squares Regressor.
+regressor_OLS = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_OLS.summary()
+
+# Phase 5
+# =======
+# Removed least significant P-value [5] found in Phase 1.
+x_opt = x[:, [0, 3]]
+
+# Ordinarily Least Squares Regressor.
+regressor_OLS = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_OLS.summary()
